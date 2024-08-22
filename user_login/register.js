535e89057ce93494a1eda9f7a7c3cc6d69bf9391
Aditkece <147034263+RexndleZero@users.noreply.github.com>
2024-08-23 00:39:48 +0700

    mengubah tampilan register dan js

--- user_login/register.js
+++ user_login/register.js
@@ -1,70 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const loginLink = document.getElementById("login-link");
-
-  loginLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.location.href =
-      "https://librobackend-production.up.railway.app/login.html";
-  });
-
   const togglePassword = document.getElementById("togglePassword");
   const password = document.getElementById("password");
-  const toggleConfirmPassword = document.getElementById(
-    "toggleConfirmPassword"
-  );
+  const toggleConfirmPassword = document.getElementById("toggleConfirmPassword");
   const confirmPassword = document.getElementById("confirm_password");
 
   togglePassword.addEventListener("click", () => {
-    const type =
-      password.getAttribute("type") === "password" ? "text" : "password";
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
     togglePassword.classList.toggle("fa-eye-slash");
   });
 
   toggleConfirmPassword.addEventListener("click", () => {
-    const type =
-      confirmPassword.getAttribute("type") === "password" ? "text" : "password";
+    const type = confirmPassword.getAttribute("type") === "password" ? "text" : "password";
     confirmPassword.setAttribute("type", type);
     toggleConfirmPassword.classList.toggle("fa-eye-slash");
   });
 
-  document
-    .getElementById("registerForm")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
+  document.getElementById("registerForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-      const username = document.getElementById("username").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      const confirmPassword = document.getElementById("confirm_password").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value;
 
-      if (password !== confirmPassword) {
-        alert("Passwords tidak cocok.");
-        return;
-      }
+    if (password !== confirmPassword) {
+      alert("Passwords tidak cocok.");
+      return;
+    }
 
-      const response = await fetch(
-        "https://librobackend-production.up.railway.app/post/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, email, password }),
-        }
-      );
-
-      const message = document.getElementById("message");
-      if (response.status === 201) {
-        message.textContent =
-          "Pendaftaran berhasil! Mengalihkan ke halaman login...";
-        setTimeout(() => {
-          window.location.href =
-            "https://librobackend-production.up.railway.app/";
-        }, 2000);
-      } else {
-        const errorData = await response.json();
-        message.textContent = `Error: ${errorData.error}`;
-      }
+    const response = await fetch("https://librobackend-production.up.railway.app/post/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, email, password }),
     });
+
+    const message = document.getElementById("message");
+    if (response.status === 201) {
+      message.textContent = "Pendaftaran berhasil! Mengalihkan ke halaman login...";
+      setTimeout(() => {
+        window.location.href = "https://librobackend-production.up.railway.app/";
+      }, 2000);
+    } else {
+      const errorData = await response.json();
+      message.textContent = `Error: ${errorData.error}`;
+    }
+  });
 });
